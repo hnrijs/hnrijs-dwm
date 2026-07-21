0647f716a8af219e2b46d4aba2a75b8827dbcad4
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-07-21 22:10:30 +0300

    Delete config/scripts/power_profile.sh

--- config/scripts/power_profile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-current=$(powerprofilesctl get)
-if [ "$current" = "balanced" ]; then
-    powerprofilesctl set performance
-elif [ "$current" = "performance" ]; then
-    powerprofilesctl set power-saver
-else
-    powerprofilesctl set balanced
-fi
