283cac5f05e6124a9d79d276ea86aa0455b90f7e
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-08-06 13:35:15 +0300

    Update installgentoo.sh

--- installgentoo.sh
+++ installgentoo.sh
@@ -99,7 +99,6 @@ thunar --daemon &
 dunst &
 polybar &
 clipmenud &
-$HOME/.config/scripts/polybar.sh &
 $HOME/.config/scripts/screen.sh &
 # GNOME Policykit agent for authentication dialogs
 /usr/libexec/polkit-gnome-authentication-agent-1 &
