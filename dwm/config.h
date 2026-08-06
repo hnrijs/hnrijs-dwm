cfd0f690e5955d0875a7691aa3e6f3762bb5e80d
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-08-07 00:04:58 +0300

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 2;
@@ -6,8 +7,8 @@ static const unsigned int snap      = 32;
 static const int showbar            = 1;
 static const int topbar             = 1;
 static const unsigned int refreshrate = 120;
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
 
 static const char col_bg[]          = "#000000";
 static const char col_fg[]          = "#888888";
@@ -23,25 +24,20 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-    /* xprop(1):
-     *    WM_CLASS(STRING) = instance, class
-     *    WM_NAME(STRING) = title
-     */
     /* class      instance    title       tags mask     isfloating   monitor */
     { "Gimp",     NULL,       NULL,       0,            1,           -1 },
     { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const float mfact     = 0.55;
+static const int nmaster     = 1;
+static const int resizehints = 1;
+static const int lockfullscreen = 1;
 
 static const Layout layouts[] = {
-    /* symbol     arrange function */
-    { "[]=",      tile },    /* first entry is default */
-    { "><>",      NULL },    /* no layout function means floating behavior */
+    { "[]=",      tile },
+    { "><>",      NULL },
     { "[M]",      monocle },
 };
 
@@ -53,11 +49,10 @@ static const Layout layouts[] = {
     { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2] = "0";
 static const char *dmenucmd[]       = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_bg, "-sf", col_fg_active, NULL };
 static const char *termcmd[]        = { "alacritty", NULL };
 static const char *roficmd[]        = { "rofi", "-show", "run", NULL };
@@ -69,34 +64,61 @@ static const char *codecmd[]        = { "vscode", NULL };
 static const char *pavucmd[]        = { "pavucontrol", NULL };
 static const char *nmtuicmd[]       = { "alacritty", "--class", "nmtui", "-e", "nmtui", NULL };
 static const char *lockcmd[]        = { "slock", NULL };
+static const char *obscmd[]         = { "obs", NULL };
+static const char *resolvecmd[]     = { "resolve", NULL };
+static const char *audaciouscmd[]   = { "audacious", NULL };
+static const char *protonvpncmd[]   = { "protonvpn-app", NULL };
+
 static const char *powermenu[]      = { "sh", "-c", "$HOME/.config/scripts/rofi-powermenu.sh", NULL };
 static const char *powerprof[]      = { "sh", "-c", "$HOME/.config/scripts/power_profile.sh", NULL };
-static const char *screenshot[]     = { "sh", "-c", "maim -s | xclip -selection clipboard -t image/png", NULL };
-static const char *screenall[]      = { "sh", "-c", "mkdir -p ~/Pictures/Screenshots && f=~/Pictures/Screenshots/scr_$(date +%s).png && maim \"$f\" && xclip -selection clipboard -t image/png -i \"$f\"", NULL };
+static const char *screenshot[]     = { "sh", "-c", "mkdir -p $HOME/Pictures/Screenshots && f=$HOME/Pictures/Screenshots/scr_$(date +%s).png && maim -s \"$f\" && xclip -selection clipboard -t image/png -i \"$f\"", NULL };
+static const char *screenall[]      = { "sh", "-c", "mkdir -p $HOME/Pictures/Screenshots && f=$HOME/Pictures/Screenshots/scr_$(date +%s).png && maim \"$f\" && xclip -selection clipboard -t image/png -i \"$f\"", NULL };
 static const char *screensrc[]      = { "sh", "-c", "$HOME/.config/scripts/screen_search.sh", NULL };
+
+/* Audio and Brightness Commands */
+static const char *upvol[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL };
+static const char *downvol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",     NULL };
+static const char *mutevol[] = { "pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle",  NULL };
+static const char *brupcmd[] = { "brightnessctl", "set", "5%+", NULL };
+static const char *brdowncmd[] = { "brightnessctl", "set", "5%-", NULL };
+
+
 
 static const Key keys[] = {
     /* modifier                     key        function        argument */
-    { MODKEY,                       XK_Return,          spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_space,           spawn,          {.v = roficmd } },
-    { MODKEY,                       XK_v,               spawn,          {.v = clipmenucmd } },
-    { MODKEY,                       XK_f,               spawn,          {.v = thunarcmd } },
-    { MODKEY,                       XK_b,               spawn,          {.v = browsercmd } },
-    { MODKEY,                       XK_s,               spawn,          {.v = comcmd } },
-    { MODKEY,                       XK_c,               spawn,          {.v = codecmd } },
-    { MODKEY|ShiftMask,             XK_a,               spawn,          {.v = pavucmd } },
-    { MODKEY|ShiftMask,             XK_n,               spawn,          {.v = nmtuicmd } },
+    { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY,                       XK_space,  spawn,          {.v = roficmd } },
+    { MODKEY,                       XK_v,      spawn,          {.v = clipmenucmd } },
+    { MODKEY,                       XK_f,      spawn,          {.v = thunarcmd } },
+    { MODKEY,                       XK_b,      spawn,          {.v = browsercmd } },
+    { MODKEY,                       XK_s,      spawn,          {.v = comcmd } },
+    { MODKEY,                       XK_c,      spawn,          {.v = codecmd } },
+    { MODKEY,                       XK_o,      spawn,          {.v = obscmd } },
+    { MODKEY,                       XK_r,      spawn,          {.v = resolvecmd } },
+    { MODKEY,                       XK_m,      spawn,          {.v = audaciouscmd } },
+    { MODKEY|ShiftMask,             XK_v,      spawn,          {.v = protonvpncmd } },
+
+    { MODKEY|ShiftMask,             XK_a,      spawn,          {.v = pavucmd } },
+    { MODKEY|ShiftMask,             XK_n,      spawn,          {.v = nmtuicmd } },
 
     /* Scripts & Power */
-    { MODKEY|ShiftMask,             XK_L,               spawn,          {.v = lockcmd } },
-    { MODKEY,                       XK_Escape,          spawn,          {.v = powermenu } },
-    { MODKEY|ShiftMask,             XK_p,               spawn,          {.v = powerprof } },
+    { MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
+    { MODKEY,                       XK_Escape, spawn,          {.v = powermenu } },
+    { MODKEY|ShiftMask,             XK_p,      spawn,          {.v = powerprof } },
 
     /* Utilities */
-    { MODKEY|ShiftMask,             XK_s,               spawn,          {.v = screenshot } },
-    { MODKEY|ShiftMask,             XK_x,               spawn,          {.v = screenall } },
-    { MODKEY|ShiftMask,             XK_e,               spawn,          {.v = screensrc } },
+    { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = screenshot } },
+    { MODKEY|ShiftMask,             XK_x,      spawn,          {.v = screenall } },
+    { MODKEY|ShiftMask,             XK_e,      spawn,          {.v = screensrc } },
 
+    /* Media Keys (Volume & Brightness) */
+    { 0, XF86XK_AudioRaiseVolume,              spawn,          {.v = upvol } },
+    { 0, XF86XK_AudioLowerVolume,              spawn,          {.v = downvol } },
+    { 0, XF86XK_AudioMute,                     spawn,          {.v = mutevol } },
+    { 0, XF86XK_MonBrightnessUp,               spawn,          {.v = brupcmd } },
+    { 0, XF86XK_MonBrightnessDown,             spawn,          {.v = brdowncmd } },
+
+    /* Window Management */
     { MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_Right,  focusstack,     {.i = +1 } },
     { MODKEY,                       XK_Left,   focusstack,     {.i = -1 } },
@@ -130,9 +152,7 @@ static const Key keys[] = {
 };
 
 /* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-    /* click                event mask      button          function        argument */
     { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
     { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
     { ClkWinTitle,          0,              Button2,        zoom,           {0} },
