0fc27e27080a81d45da75ac6023922ae2fc5b4b6
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-08-06 04:27:08 +0300

    Update installgentoo.sh

--- installgentoo.sh
+++ installgentoo.sh
@@ -29,7 +29,7 @@ sudo emerge --ask=n --noreplace --binpkg-respect-use=y \
     media-gfx/maim sys-power/power-profiles-daemon x11-misc/picom \
     x11-misc/lightdm x11-misc/lightdm-gtk-greeter www-client/firefox-bin \
     media-video/pipewire media-video/wireplumber \
-    sys-apps/xdg-desktop-portal sys-apps/xdg-desktop-portal-gtk
+    sys-apps/xdg-desktop-portal sys-apps/xdg-desktop-portal-gtk lxappearance
 
 # 4. Copy configuration files to ~/.config
 echo "Copying config files to $HOME/.config/..."
